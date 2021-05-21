3940eb513b72b0e661123a30f3018eff093f4350
YanivBohbot <Yanivbohbot5@gmail.com>
2021-05-21 15:31:57 +0300

    add sidebar and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dashboard';
+  
+  
+  title = 'Bussiness INSIGHT';
+ 
+  subtitle="dashboard BI";
+
+
 }
