b3f7a31b3d6ecbd085f469c203de03fc35f8d064
YanivBohbot <Yanivbohbot5@gmail.com>
2021-05-21 18:43:19 +0300

    add pie chart data

--- src/app/Charts/pie-chart/pie-chart.component.ts
+++ src/app/Charts/pie-chart/pie-chart.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType, ChartOptions } from 'chart.js';
+import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
+
+
 
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
   styleUrls: ['./pie-chart.component.css']
 })
+
 export class PieChartComponent implements OnInit {
+  // Pie
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public pieChartLabels: Label[] = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
+  public pieChartData: SingleDataSet = [300, 500, 100];
+  public pieChartType: ChartType = 'doughnut';
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
 
-  constructor() { }
-
+  constructor() {
+    monkeyPatchChartJsTooltip();
+    monkeyPatchChartJsLegend();
+  }
+  
   ngOnInit(): void {
   }
 
