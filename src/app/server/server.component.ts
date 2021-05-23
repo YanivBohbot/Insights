eab362a5d21d604f93c7adcebe333bbe242b36bd
YanivBohbot <Yanivbohbot5@gmail.com>
2021-05-23 12:00:50 +0300

    updated server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,12 +7,33 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
-
+   color: string;
+   buttonText:string;
 
    @Input() serverInput;
   constructor() { }
 
   ngOnInit(): void {
+
+    this.getServerAction(this.serverInput.isOnline);
+  }
+
+
+  getServerAction(isOnline: boolean){
+    if( isOnline){
+      this.serverInput.isOnline = true;
+      this.color = '#66BB6A';
+      this.buttonText='Shut Down';
+    } else{
+      this.serverInput.isOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText='Start';
+    }
+  }
+
+  togglestatus( onlinestatus: boolean){
+    this.getServerAction( !onlinestatus)
+    // this.serverInput.isOnline  = !this.serverInput.isOnline;
   }
 
 }
